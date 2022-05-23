3dd8e33952b21fdcbb4697aaf6b229232b88d682
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-05-23 18:47:40 +0700

    feat: done copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
 
+function copyDir() {
+  
+  const fs = require('fs');
+  const path = require('path');
+  const newFolderPath = path.join(__dirname, 'files-copy');
+  const copyFolderPath = path.join(__dirname, 'files');
+
+  fs.mkdir(newFolderPath, {recursive: true}, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(copyFolderPath, (err, files) => {
+    for (let file of files) {
+      if (err) throw err;
+      let folderFile = path.join(__dirname, 'files', file);
+      let newFolderPathFiles = path.join(__dirname, 'files-copy', file);
+      fs.copyFile(folderFile, newFolderPathFiles, (err) => {
+        if (err) throw err;
+      });
+    }
+    console.log('Files have been copied to a new folder');
+  });
+
+}
+
+copyDir();
